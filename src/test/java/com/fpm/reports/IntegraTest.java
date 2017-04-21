5b06e7ebb7008e5b9fc774825d7eee05efa23ab8
feech <andreev.kirill.o@gmail.com>
2017-04-21 07:45:02 +0200

    add integration test

---
+++ src/test/java/com/fpm/reports/IntegraTest.java
@@ -0,0 +1,19 @@
+package com.fpm.reports;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Kirill on 4/21/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegraTest {
+
+    @Test
+    public void contextUpTest(){
+
+    }
+}
